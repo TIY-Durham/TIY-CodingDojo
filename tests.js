5402c6c63ccb88b1bd2c6b3296c92daa0b1158df
David Rogers <al.the.x@gmail.com>
2015-10-11 21:45:02 -0400

    Let us start with a plan...

--- tests.js
+++ tests.js
@@ -7,4 +7,20 @@ var test = require('mocha').it,
 test('toFraction', function(){
   // Does this thing even exist?
   expect(main.toFraction).to.be.a('function');
+
+  /**
+   * toFraction accepts Numbers with two (2) decimal places
+   * and any whole number value and produces a String
+   * representation of JUST the decimal as a fraction.
+   *
+   * input | output
+   * ==============
+   *  0.00 | "00/100"
+   *  0.01 | "01/100"
+   *  1.23 | "23/100"
+   *  0    | "00/100"
+   *  0.99 | "99/100"
+   *  1.00 | "00/100"
+   * 3.349 | "34/100" or "35/100" ???
+   */
 }); // END toFraction
