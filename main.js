95eaf34a03b3de853ec07902d94e301dab91ae53
David Rogers <al.the.x@gmail.com>
2015-10-11 21:47:46 -0400

    The simplest thing that could possibly work.

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
   globals.toFraction = toFraction;
 
   function toFraction(number){
+    if (number == 0.00){
+      return '00/100';
+    }
+
+    if (number == 0.01){
+      return '01/100';
+    }
+
+    return '02/100';
   }
 })(module && module.exports || window || this);
 // Now we know a little more...
