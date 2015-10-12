88d2727254dcae08be551991da5f8fb8deadc38f
David Rogers <al.the.x@gmail.com>
2015-10-11 21:37:53 -0400

    Reset to empty... more or less

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 ;(function(globals){ // That IIFE though...
-  // In Node JS: module.exports.hello = hello;
-  // In Browser: window.hello = hello;
-  globals.hello = hello;
-
-  // In Node JS: module.exports.hello();
-  // In Browser: window.hello();
-  globals.hello();
-
-  function hello(){
-    console.log('Hello from main.js!');
-  }
 })(module && module.exports || window || this);
 // Now we know a little more...
